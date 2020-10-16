69bfe5d93ea5dc675d590b278268fef70c610b6f
AHollierDS <alexandre.hollier@gadz.org>
2020-10-16 12:58:54 +0200

    Script for lightGBM

---
+++ 01_feature_engineering/scripts/light.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+
+from lightgbm import LGBMClassifier
+from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.model_selection import KFold, StratifiedKFold
+
+
+def kfold_lightgbm(df, num_folds, stratified = False, debug= False):
+    
+    # Divide in training/validation and test data
+    train_df = df[df['TARGET'].notnull()]
+    test_df = df[df['TARGET'].isnull()]
+    print("Starting LightGBM. Train shape: {}, test shape: {}".format(train_df.shape, test_df.shape))
+    del df
+    #gc.collect()
+    
+    # Cross validation model
+    if stratified:
+        folds = StratifiedKFold(n_splits= num_folds, shuffle=True, random_state=47)
+    else:
+        folds = KFold(n_splits= num_folds, shuffle=True, random_state=47)
+        
+    # Create arrays and dataframes to store results
+    oof_preds = np.zeros(train_df.shape[0])
+    sub_preds = np.zeros(test_df.shape[0])
+    feature_importance_df = pd.DataFrame()
+    feats = [f for f in train_df.columns if f not in ['TARGET','SK_ID_CURR','SK_ID_BUREAU','SK_ID_PREV','index']]
+    
+    for n_fold, (train_idx, valid_idx) in enumerate(folds.split(train_df[feats], train_df['TARGET'])):
+        train_x, train_y = train_df[feats].iloc[train_idx], train_df['TARGET'].iloc[train_idx]
+        valid_x, valid_y = train_df[feats].iloc[valid_idx], train_df['TARGET'].iloc[valid_idx]
+
+        # LightGBM parameters found by Bayesian optimization
+        clf = LGBMClassifier(
+            nthread=4,
+            #is_unbalance=True,
+            n_estimators=10000,
+            learning_rate=0.02,
+            num_leaves=32,
+            colsample_bytree=0.9497036,
+            subsample=0.8715623,
+            max_depth=8,
+            reg_alpha=0.04,
+            reg_lambda=0.073,
+            min_split_gain=0.0222415,
+            min_child_weight=40,
+            silent=-1,
+            verbose=-1
+            #scale_pos_weight=11
+            )
+
+        clf.fit(
+            train_x, train_y, eval_set=[(train_x, train_y), (valid_x, valid_y)], 
+            eval_metric= 'auc', verbose= 100, early_stopping_rounds= 200
+        )
+
+        oof_preds[valid_idx] = clf.predict_proba(valid_x, num_iteration=clf.best_iteration_)[:, 1]
+        sub_preds += clf.predict_proba(test_df[feats], num_iteration=clf.best_iteration_)[:, 1] / folds.n_splits
+
+        fold_importance_df = pd.DataFrame()
+        fold_importance_df["feature"] = feats
+        fold_importance_df["importance"] = clf.feature_importances_
+        fold_importance_df["fold"] = n_fold + 1
+        feature_importance_df = pd.concat([feature_importance_df, fold_importance_df], axis=0)
+        print('Fold %2d AUC : %.6f' % (n_fold + 1, roc_auc_score(valid_y, oof_preds[valid_idx])))
+        del clf, train_x, train_y, valid_x, valid_y
+        #gc.collect()
+
+    print('Full AUC score %.6f' % roc_auc_score(train_df['TARGET'], oof_preds))
